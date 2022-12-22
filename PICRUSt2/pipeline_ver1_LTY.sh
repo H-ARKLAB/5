70172b03d40f98bd07f86ba84c9133dd45156b40
Taeyoung Lee <102836650+tylee303@users.noreply.github.com>
2022-12-22 17:28:43 +0900

    Update pipeline_ver1_LTY.sh

--- PICRUSt2/pipeline_ver1_LTY.sh
+++ PICRUSt2/pipeline_ver1_LTY.sh
@@ -29,6 +29,40 @@ chmod +x 1_phylogenetictee_with_ASV.sh
 place_seqs.py -s ../exported-seqs-table/dna-sequences.fna -o out.tre -p 16 \
               --intermediate intermediate/place_seqs
 
+# step 2. Hidden-state prediction of gene families
+## filename: 2_gene_prediction.sh
 
+hsp.py -i 16S -t out.tre -o marker_predicted_and_nsti.tsv.gz -p 16 -n
+hsp.py -i EC -t out.tre -o EC_predicted.tsv.gz -p 16
 
+# take a look at each file
+zless -S marker_predicted_and_nsti.tsv.gz
+zless -S EC_predicted.tsv.gz
+
+# step 3. Generate metagenome predictions
+## filename: 3_metagenome_prediction.sh
+
+metagenome_pipeline.py -i ../feature-table.biom -m marker_predicted_and_nsti.tsv.gz -f EC_predicted.tsv.gz \
+                       -o EC_metagenome_out --strat_out
+
+## (optional) generate this file in legacy format for programs like BURRITO and MIMOSA
+
+convert_table.py EC_metagenome_out/pred_metagenome_contrib.tsv.gz \
+                 -c contrib_to_legacy \
+                 -o EC_metagenome_out/pred_metagenome_contrib.legacy.tsv.gz
+
+# step 4. Pathway-level inference
+## filename: 4_pathway_inference.sh
+
+pathway_pipeline.py -i EC_metagenome_out/pred_metagenome_unstrat.tsv.gz \
+                    -o pathways_out -p 16
+
+# step 5. Add functional descriptions
+## filename: 5_add_functional_description.sh
+
+add_descriptions.py -i EC_metagenome_out/pred_metagenome_unstrat.tsv.gz -m EC \
+                    -o EC_metagenome_out/pred_metagenome_unstrat_descrip.tsv.gz
+
+add_descriptions.py -i pathways_out/path_abun_unstrat.tsv.gz -m METACYC \
+                    -o pathways_out/path_abun_unstrat_descrip.tsv.gz
 
